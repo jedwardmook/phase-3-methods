67b4b8029ec1d049ad27eec2dc05457c6c071d9a
JedwardMook <jedwardmook@yahoo.com>
2022-05-30 20:26:53 -0500

    commit tree b32fb351ede38a126c4e5e12baab5c675c064a82

--- spec/methods_spec.rb
+++ spec/methods_spec.rb
@@ -52,3 +52,27 @@ describe '#halve' do
   end
 
 end
+
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+def greet name 
+  puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+  puts "Hello, #{name}!"
+end
+
+def add num1, num2
+  num1 + num2;
+end
+
+def halve number
+  if number.is_a?(Integer)
+    number / 2
+  else
+    nil
+  end
+end
